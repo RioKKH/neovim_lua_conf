16b5348fa377c833fc81fa8a4ba48cfeb29a9882
Ryoichi <ryoichikakehi@gmail.com>
2024-11-12 04:56:58 +0900

    スニペット追加 	modified:   init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,7 @@
 require("config.lazy")
-
 -- leaderキーの設定
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
 -- プラグインの設定
 require("lazy").setup({
   -- mason
@@ -30,7 +28,12 @@ require("lazy").setup({
       })
     end,
   },
-
+  {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
   -- 補完
   {
     "hrsh7th/nvim-cmp",
@@ -41,7 +44,46 @@ require("lazy").setup({
       "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
+    config = function()
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      -- スニペット間の移動用のキーマッピング
+      -- instertモード(i)とselectモード(s)で有効になるキーマッピング
+      vim.keymap.set({ "i", "s" }, "<Tab>", function()
+        -- スニペットが展開可能な位置にいるか
+        if luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "")
+        end
+      end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, { silent = true })
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- LuaSnipソースを追加
+          { name = "buffer" }, -- バッファからの補完
+          { name = "path" }, -- パス補完
+        },
+      })
+    end,
   },
   -- none-ls関連
   {
@@ -54,7 +96,6 @@ require("lazy").setup({
       local null_ls = require("none-ls")
       local formatting = null_ls.builtins.formatting
       local diagnostics = null_ls.builtins.diagnostics
-
       null_ls.setup({
         sources = {
           formatting.black,
@@ -84,25 +125,12 @@ require("lazy").setup({
   },
 })
 
--- 補完の設定
-local cmp = require("cmp")
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = {
-    { name = "nvim_lsp" },
-  },
-})
-
 -- LSPの設定
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 -- Python LSP
 require("lspconfig").pyright.setup({
   capabilities = capabilities,
 })
-
 -- C/C++ LSP
 require("lspconfig").clangd.setup({
   capabilities = capabilities,
