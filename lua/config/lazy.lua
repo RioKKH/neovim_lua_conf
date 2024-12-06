b5f39fa5aefbedc97a829f302c26e24bc349fc0a
Ryoichi <ryoichikakehi@gmail.com>
2024-12-06 22:38:07 +0900

    エラーが出ないようにsetup()を呼び出す回数を1回になるように修正 	new file:   .gitignore 	new file:   LICENSE 	new file:   README.md 	modified:   init.lua 	new file:   lua/config/autocmds.lua 	new file:   lua/config/keymaps.lua 	new file:   lua/config/lazy.lua 	new file:   lua/config/options.lua 	new file:   lua/plugins/example.lua 	new file:   stylua.toml

---
+++ lua/config/lazy.lua
@@ -0,0 +1,182 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+  change_detection = {
+    notify = false,
+  },
+  -- mason
+  {
+    "williamboman/mason.nvim",
+    priority = 100, -- 優先度を高く設定
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "pyright", -- Python
+          "clangd", -- C/C++
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "rafamadriz/friendly-snippets",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+  -- 補完
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      -- スニペット間の移動用のキーマッピング
+      -- instertモード(i)とselectモード(s)で有効になるキーマッピング
+      vim.keymap.set({ "i", "s" }, "<Tab>", function()
+        -- スニペットが展開可能な位置にいるか
+        if luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "")
+        end
+      end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, { silent = true })
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- LuaSnipソースを追加
+          { name = "buffer" }, -- バッファからの補完
+          { name = "path" }, -- パス補完
+        },
+      })
+    end,
+  },
+  -- none-ls関連
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local null_ls = require("none-ls")
+      local formatting = null_ls.builtins.formatting
+      local diagnostics = null_ls.builtins.diagnostics
+      null_ls.setup({
+        sources = {
+          formatting.black,
+          -- diagnostics.flake8,
+          formatting.ruff,
+          diagnostics.ruff,
+          formatting.clang_format,
+        },
+      })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          "black",
+          -- "flake8",
+          "ruff",
+          "clang-format",
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+})
