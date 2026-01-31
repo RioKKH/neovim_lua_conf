af52341d444b545933f032cc18aeca8a019da540
Ryoichi <ryoichikakehi@gmail.com>
2026-01-31 17:47:06 +0900

    OSC 52を用いてSSH経由でクリップボード共有が出来るようにした 	modified:   lua/config/options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,28 @@
 -- マウスモードをオフにする
 vim.o.mouse = ""
 
-vim.o.clipboard = "unnamedplus"
+-- これはローカルでのクリップボードプロバイダ (xclip等)向けの設定
+-- vim.o.clipboard = "unnamedplus"
+
+-- OSC 52を使ってSSH経由でもローカルPCにコピー可能とする
+local function paste()
+    return {
+        vim.fn.split(vim.fn.getreg(""), "\n"),
+        vim.fn.getregtype(""),
+    }
+end
+
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["+"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = paste,
+        ["+"] = paste,
+    },
+}
+
+-- yでヤンクしたときに+レジスタ(システムクリップボード)を使用する
+vim.opt.clipboard:append("unnamedplus")
